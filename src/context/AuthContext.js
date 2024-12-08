d03f8ee72b6edf0fd042a55ade1eb735998ade36
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-12-08 15:41:25 +0530

    https added

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -69,6 +69,7 @@ export const AuthProvider = ({ children }) => {
   const logout = () => {
     setUser(null);
     localStorage.removeItem('auth_token');
+    
     localStorage.removeItem('role');
   };
 
